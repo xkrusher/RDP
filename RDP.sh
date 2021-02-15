daef8472d6a8ce854e178d496767afb076afc557
alok676875 <74966945+alok676875@users.noreply.github.com>
2021-02-15 22:39:03 +0530

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -3,7 +3,7 @@ printf "Check Out My Channel While Waiting- https://youtu.be/VeO22kIvZOQ" >&2
 {
 sudo useradd -m ALOK
 sudo adduser ALOK sudo
-echo 'ALOK:2520' | sudo chpasswd
+echo 'ALOK:8426' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
@@ -26,7 +26,7 @@ printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debia
 read -p "Paste Here: " CRP
 su - ALOK -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n'
-printf 'Your SUDO Pasword Is 2520 \n\n\n'
+printf 'Your SUDO Pasword Is 8426 \n\n\n'
 printf 'Upgrading... \n\n'
 if sudo apt-get upgrade &> /dev/null
 then
