f5812daeeb4ca68e1f2b0d4a26d5c97c550e66ee
alok676875 <74966945+alok676875@users.noreply.github.com>
2021-02-15 21:21:25 +0530

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -20,12 +20,13 @@ sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
 sudo adduser ALOK chrome-remote-desktop
 } &> /dev/null &&
-printf "\nSetup Complete " >&2 ||
+printf "\nSetup Completed " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
 su - ALOK -c """$CRP"""
-printf 'Check https://remotedesktop.google.com/access/ \n\n'
+printf 'Check https://remotedesktop.google.com/access/ \n'
+printf 'Upgrading... \n\n'
 if sudo apt-get upgrade &> /dev/null
 then
     printf "\n\nUpgrade Completed " >&2
