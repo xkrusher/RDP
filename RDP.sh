5abed312bb9cf4e04f8e3a86f1a63eb95279120a
alok676875 <74966945+alok676875@users.noreply.github.com>
2021-02-25 22:59:52 +0530

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -17,6 +17,9 @@ sudo systemctl disable lightdm.service
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
+wget https://media.codeweavers.com/pub/crossover/cxlinux/demo/crossover_20.0.2-1.deb
+sudo dpkg -i crossover_20.0.2-1.deb
+sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
 sudo apt -y install obs-studio
 sudo apt -y install firefox
